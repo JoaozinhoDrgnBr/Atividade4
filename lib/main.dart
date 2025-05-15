627bcd0b3c62a2e96b2e901ff40ed1f01920386b
Felipe Campos <felipemoraisoc@gmail.com>
2025-05-15 00:22:41 -0300

    Onboarding corrigido

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:atividade4/screens/home/home_screen.dart';
+import 'package:atividade4/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'Modules/splash_screen.dart';
 import 'Modules/onboarding_screen.dart';
-import 'Modules/login_screen.dart';
-import 'screens/login/login_screen.dart';
 import 'shared/constants/constants.dart';
 
 void main() {
@@ -12,155 +12,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App de Login',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.background,
-        primaryColor: AppColors.primary,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: AppColors.primary,
-          secondary: AppColors.primary,
-        ),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(color: AppColors.darkText),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          hintStyle: TextStyle(color: Colors.grey),
-        ),
-      ),
-      home: const LoginScreen(),
-    );
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Splash Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, primaryColor: Colors.blue),
       initialRoute: '/',
       routes: {
-        '/': (context) => const SplashScreen(
+        '/':
+            (context) => const SplashScreen(
               nextRoute: '/onboarding',
               lottiePath: 'assets/animations/splash_animation.json',
             ),
-        '/onboarding': (context) => const OnboardingScreen(
-              loginRoute: '/login',
-            ),
-        '/login': (context) => const LoginScreen(
-              homeRoute: '/home',
-            ),
-        '/home': (context) => const HomeScreen(),
+        '/onboarding':
+            (context) => const OnboardingScreen(loginRoute: '/login'),
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'Bem-vindo ao aplicativo!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
     );
   }
 }
